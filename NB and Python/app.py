43cb5f5df6d4c5ed0a268a4024979a77ab792322
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-05-26 13:30:44 -0400

    DONE

--- NB and Python/app.py
+++ NB and Python/app.py
@@ -7,29 +7,19 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-#################################################
-# Database Setup CHANGE WHERE THE DATABASE POINTS TO
-#################################################
-
+#### Database Setup CHANGE WHERE THE DATABASE POINTS TO ####
 engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
 Base = automap_base()
-# # reflect the tables
 Base.prepare(engine, reflect=True)
 #print(Base.classes.keys())
-# # Save reference to the table
 measurements = Base.classes.measurement 
 stations = Base.classes.station
 
-# #################################################
-# # Flask Setup
-# #################################################
+#### Flask Setup ####
 app = Flask(__name__)
 
-# #################################################
-# # Flask Routes DONT CHANGE ANYTHING ABOVE THIS
-# #################################################
+#### Flask Routes ####
 
 @app.route("/")
 def welcome():
@@ -40,16 +30,12 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         #f"/api/v1.0/&lt;start&gt;<br/>" #ACCORDING TO ASK BCS
-        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt; [NOTE: FORMAT IS yyyy-mm-dd] <br/>"
     )
 
-# #################################################
-# # DONT CHANGE ANYTHING ABOVE THIS
-# #################################################
+#### DONT CHANGE ANYTHING ABOVE THIS ONCE IT IS WORKING ####
     
 ################ PRECIP ##############
-# Convert query results to a dict using date as key and prcp as value
-# Return the json rep of the dict
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -68,7 +54,7 @@ def precipitation():
 
     return jsonify(all_precip) #### this is the final step of the method that started with def precipitaiton(): ####
 
-########### ALL STATIONS ####################
+################ ALL STATIIONS ##############
 @app.route("/api/v1.0/stations")
 def stations2():
     session = Session(engine)
@@ -84,8 +70,7 @@ def stations2():
 
     return jsonify(all_stations)
 
-############ MOST ACTIVE #########################
-# date/temp of most active for last year 
+################ MOST ACTIVE ##############
 
 @app.route("/api/v1.0/tobs")
 def tobias_funke():
@@ -103,88 +88,27 @@ def tobias_funke():
 
     return jsonify(so_very_active) #### this is the final step of the method that started with def precipitaiton(): ####
 
-############ START ROUTE ###############
-# Route accepts the start date as a parameter from the URL
-# Returns min, max, and avg temp calculated from given start date to end of the dataset
+################ START/END ##############
+
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
-def whatever(start=None, end=None):
-    session = Session(engine)
-    sel = [func.min(measurements.tobs), func.avg(measurements.tobs), func.max(measurements.tobs)]
+
+def start_end(start=None, end=None): 
+    session = Session(engine) 
+    three_temps = [func.min(measurements.tobs), func.max(measurements.tobs), func.avg(measurements.tobs)]
     if not end:
-       results = session.query(*sel).\
+       results = session.query(*three_temps).\
        filter(measurements.date <= start).all()
        temps = list(np.ravel(results))
        return jsonify(temps)
 
-    results = session.query(*sel).\
+    results = session.query(*three_temps).\
     filter(measurements.date >= start).\
     filter(measurements.date <= end).all()
     temps = list(np.ravel(results))
 
     return jsonify(temps=temps)
-    # session = Session(engine)
-    # results = session.query(stations.name, stations.station, measurements.date).filter(measurements.date >= start).all()
-    # session.close()
-
-    # cats=[]
-
-    # for name, station, date in results:
-    #     cats_dict={}
-    #     cats_dict[date] = station, name 
-    #     cats.append(cats_dict)
-
-    # return jsonify(cats)
-
-
-
-
-
-
-############# START/END ROUTE ##############
-# Route accepts the start and end dates as parameters from the URL
-# Returns min, max, and avg temp calculated from given start date to end of the dataset
-#@app.route("/api/v1.0/start/end")
-
-
-
-
-
-
-
 
 #THIS GOES AT THE VERY, VERY END!  IF NOT IT WILL TERMINATE THE API OR WHATEVER IT IS EARLY
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-################THIS IS A TEMPLATE ##############
-################# DELETE THIS WHEN DONE ##############
-# @app.route("/api/v1.0/precipitation")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-#     session.close()
-
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
-
-#     return jsonify(all_passengers)
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
